697b5d70f2b5084d7e83e007a0607db631064cba
Hesham Abdulzaher <heshamabdulzaher@gmail.com>
2017-10-08 10:55:36 +0200

    commpressed all real images and edit animation button (book)

--- js/plugins.js
+++ js/plugins.js
@@ -28,57 +28,17 @@ $(document).ready(function(){
       heightWindow = $(window).height(),
       heightSide = heightWindow - heightNav
   $('.sidebar').css({'top': heightNav, 'height': heightSide})
-  console.log( $ )
-
-  // button book in single flat page
-  $('.btn-book').css({'left': '50%', 'opacity': '1'})
 })
 // End Sidebar
 $(window).scroll(function(){
   var scrollTop = $(window).scrollTop()
-  if( scrollTop < 80 ){
-    $('.info-about-flat').css('margin-bottom', '150px')
-    $('.btn-book').css({
-      'top': '280px',
-      'left': '50%',
-      'transition': 'all .8s ease',
-      'width': '236px',
-      'height': '61px',
-      'position': 'absolute',
-      'box-shadow':'0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)'
-    })
-    $('.btn-book span').css({
-      'padding': '8px 16px',
-      'margin': '0 1px',
-      'font-size': '30px'
-    })
-    $('.btn-book span:first-of-type').css('margin-left', '12px')
-    $('.btn-book span:last-of-type').css('margin-right', '10px')
-  }
-    // if window > 100
 
-
-  if( scrollTop > 100 ){
-    $('.info-about-flat').css('margin-bottom', '50px')
-    $('.btn-book').css({
-      'top': '0',
-      'left': '92%',
-      'transition': 'all 1s ease',
-      'width': '190px',
-      'height': '52px',
-      'box-shadow':'none',
-      'position': 'fixed'
-    })
-    $('.btn-book span').css({
-      'padding': '3px 13px',
-      'margin': '0 0.5px 0 0',
-      'font-size': '26px',
-      'font-weight': '400',
-      'text-shadow': '2px 1px 4px'
-    })
-    $('.btn-book span:first-of-type').css('margin-left', '8px')
-    $('.btn-book span:last-of-type').css('margin-right', '5px')
-  }
-  // if window > 101
-  // if()
+  // if( scrollTop > 50 ){
+  //   $('.btn-book').css({
+  //     'position': 'absolute',
+  //     'top': '0',
+  //     'right': '0',
+  //     'background-color': '$green-color'
+  //   })
+  // }
 })
